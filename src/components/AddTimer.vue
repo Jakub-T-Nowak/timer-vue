130c2e35291d8d605c478f9b8a4494f104891a48
Jakub Nowak <Jnow87@gmail.com>
2021-02-18 19:19:38 +0100

    init

---
+++ src/components/AddTimer.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-card class="add-timer">
+    <v-row>
+      <v-col cols="4">
+        <v-btn
+        fab
+        dark
+        color="indigo"
+        style="margin-top: 10px; margin-left: 10px; margin-bottom: 10px;"
+        @click="clickBtn()"
+      >
+        <v-icon dark>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+      </v-col>
+      <v-col cols="5">
+        <v-datetime-picker
+          label="Select Datetime" 
+          v-model="datetime"
+        >
+          <template slot="dateIcon">
+            <v-icon dark>
+              mdi-calendar-month
+            </v-icon>
+          </template>
+          <template slot="timeIcon">
+            <v-icon dark>
+              mdi-clock-outline
+            </v-icon>
+          </template>
+        </v-datetime-picker>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'AddTimer',
+
+  props: {
+    msg: String
+  },
+
+  data() {return {
+    datetime: "",
+  }},
+
+  methods:{
+    clickBtn: function () {
+      const date = moment(this.datetime);
+      const now = moment();
+      const timeValidation = moment(date).isBefore(now);
+
+      if (timeValidation) {
+
+      }
+      else {
+        
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.add-timer {
+  width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+
+import DatetimePicker from 'vuetify-datetime-picker' 
+Vue.use(DatetimePicker)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
